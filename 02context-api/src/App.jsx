00993aa32d70bb3a0d608346dcecac10f75b84d2
sabberrahman <sabbersr1@gmail.com>
2024-01-13 15:36:15 +0600

    Theme switcher

--- 02context-api/src/App.jsx
+++ 02context-api/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,17 @@ function App() {
 
   return (
     <>
-      <h1>hello ji</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                    {/* theme */}
+                </div>
+
+                <div className="w-full max-w-sm mx-auto">
+                  {/* cards */}
+                </div>
+            </div>
+      </div>
     </>
   )
 }
