0ab5809fc9c015db89380ecd107cf10551be71b0
sabberrahman <sabbersr1@gmail.com>
2024-01-13 16:49:36 +0600

    finalize project

--- 02context-api/src/App.jsx
+++ 02context-api/src/App.jsx
@@ -1,23 +1,39 @@
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import { useEffect } from 'react'
+import ThemeBtn from './componets/Themebtn'
+import Card from './componets/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [themeMode ,setThemeMode]=useState("light")
+
+ const lightTheme =()=>{
+  setThemeMode("light")
+ }
+ const darkTheme =()=>{
+  setThemeMode("dark")
+ }
+
+ useEffect(()=>{
+  document.querySelector("html").classList.remove("light","dark")
+  document.querySelector("html").classList.add(themeMode)
+ },[themeMode])
 
   return (
-    <>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
             <div className="w-full">
                 <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                    {/* theme */}
+                    <ThemeBtn/>
                 </div>
 
                 <div className="w-full max-w-sm mx-auto">
-                  {/* cards */}
+                  <Card/>
                 </div>
             </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
